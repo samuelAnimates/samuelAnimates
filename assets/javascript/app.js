438a30cd00fa7097b0c4564d400dd34d221d2f6a
smendez92 <samuelremndez14@gmail.com>
2017-07-23 14:32:11 -0500

    navbar and header stickiness worked out

---
+++ assets/javascript/app.js
@@ -0,0 +1,59 @@
+var isNavDisplayed = false;
+
+//The navigation bar slides in/out once the trigger is clicked
+$( "#navbar-modal-trigger" ).click(function() {
+
+	if (!isNavDisplayed){
+
+		//Display navbar if it is not already displayed
+		$( "#navbar-modal" ).animate(
+	    	
+	    	{ left: 0 },
+		    'slow',
+		    'swing',
+
+		);
+
+		 $("body").addClass("modal-open");
+
+		isNavDisplayed = true;
+
+	}
+
+	else{
+
+		//Hide navbar if it is already displayed
+		$( "#navbar-modal" ).animate(
+	    	
+	    	{ left: '-130%' },
+		    'slow',
+		    'swing',
+
+		);
+
+		$("body").removeClass("modal-open");
+
+		isNavDisplayed = false;
+
+	}
+
+});
+
+//The navigation bar slides out to the left once a navigation link is clicked
+$( ".navbar-link" ).click(function() {
+
+	//Hide navbar
+	$( "#navbar-modal" ).animate(
+	    	
+    	{ left: '-130%' },
+	    'slow',
+	    'swing',
+
+	);
+
+	$("body").removeClass("modal-open");
+
+	isNavDisplayed = false;
+
+
+});
