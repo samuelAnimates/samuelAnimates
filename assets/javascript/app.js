3640cd3b050b15e50d447a4da8a9c5adb16e6fdd
smendez92 <samuelremndez14@gmail.com>
2017-07-23 16:44:32 -0500

    fixed navbar and connect bar simultaneous display issue. Contact form orks but needs formatting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,28 @@
 var isNavDisplayed = false;
+var isConnectDisplayed = false;
 
-//The navigation bar slides in/out once the trigger is clicked
-$( "#navbar-modal-trigger" ).click(function() {
+function hideConnectModal(){
 
-	if (!isNavDisplayed){
+	//Hide Conncet Modal if it is already displayed
+	$( "#connect-footer-modal" ).animate(
+    	
+    	{ right: '-110%' },
+	    'slow',
+	    'swing',
 
-		//Display navbar if it is not already displayed
-		$( "#navbar-modal" ).animate(
-	    	
-	    	{ left: 0 },
-		    'slow',
-		    'swing',
+	);
 
-		);
+	$("body").removeClass("modal-open");
 
-		 $("body").addClass("modal-open");
+	isConnectDisplayed = false;
+	
+}
 
-		isNavDisplayed = true;
+function hideNavBar(){
 
-	}
-
-	else{
-
-		//Hide navbar if it is already displayed
-		$( "#navbar-modal" ).animate(
-	    	
-	    	{ left: '-130%' },
-		    'slow',
-		    'swing',
-
-		);
-
-		$("body").removeClass("modal-open");
-
-		isNavDisplayed = false;
-
-	}
-
-});
-
-//The navigation bar slides out to the left once a navigation link is clicked
-$( "navbar-individual-link-container" ).click(function() {
-
-	//Hide navbar
+	//Hide navbar if it is already displayed
 	$( "#navbar-modal" ).animate(
-	    	
+    	
     	{ left: '-130%' },
 	    'slow',
 	    'swing',
@@ -55,5 +33,101 @@ $( "navbar-individual-link-container" ).click(function() {
 
 	isNavDisplayed = false;
 
+};
 
+function showConnectModal(){
+
+	//Display Connect modal if it is not already displayed
+	$( "#connect-footer-modal" ).animate(
+    	
+    	{ right: 0 },
+	    'slow',
+	    'swing',
+
+	);
+
+	 $("body").addClass("modal-open");
+
+	isConnectDisplayed = true;
+
+}
+
+function showNavBar(){
+
+	//Display navbar if it is not already displayed
+	$( "#navbar-modal" ).animate(
+    	
+    	{ left: 0 },
+	    'slow',
+	    'swing',
+
+	);
+
+	 $("body").addClass("modal-open");
+
+	isNavDisplayed = true;
+
+};
+
+
+
+//The navigation bar slides in/out once the trigger is clicked
+$( "#navbar-modal-trigger" ).click(function() {
+
+	if (!isNavDisplayed && !isConnectDisplayed){
+
+		showNavBar();
+
+	}
+
+	else if (!isNavDisplayed && isConnectDisplayed){
+
+		hideConnectModal();
+		showNavBar();
+
+	}
+
+	else if (isNavDisplayed){
+
+		hideNavBar();
+
+	}
+
+});
+
+//The navigation bar slides back out to the left once a navigation link is clicked
+$( ".navbar-individual-link-container" ).click(function() {
+
+	hideNavBar();
+
+});
+
+//The Connect modal slides in/out once the trigger is clicked
+$( "#connect-modal-trigger" ).click(function() {
+
+	if (!isConnectDisplayed && !isNavDisplayed){
+
+		showConnectModal();
+
+	}
+
+	else if (!isConnectDisplayed && isNavDisplayed){
+
+		hideNavBar();
+		showConnectModal();
+
+	}
+
+	else if (isConnectDisplayed){
+		
+		hideConnectModal();
+
+	}
+
+});
+
+//Show message after Contact Form has been submitted
+$('#gform').on('submit', function(e) {
+  $('#gform *').fadeOut(2000);
+  $('#gform').prepend('Thank you! Talk to you soon...');
 });
